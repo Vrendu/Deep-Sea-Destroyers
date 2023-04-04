00c6e395d4b2b48251a0b108711b2770db2b9ff6
Vamshi Renduchintala <vrenduchintala17@gmail.com>
2023-04-03 21:46:43 -0700

    update enemy projectile image

--- src/scripts/projectile.js
+++ src/scripts/projectile.js
@@ -8,23 +8,26 @@ class Projectile extends GameObject {
     constructor(options) {
         options.radius = Projectile.RADIUS;
         super(options);
-        this.spriteSheet = new Image();
-        this.spriteSheet.src = "assets/flame.png";
+        this.userProjectileSheet = new Image();
+        this.userProjectileSheet.src = "assets/flame.png";
+        this.enemyProjectileSheet = new Image();
+        this.enemyProjectileSheet.src = "assets/power.png";
         this.currentFrame = 0;
-        this.frameWidth = 128; // Width of each frame in pixels
-        this.frameHeight = 128; // Height of each frame in pixels
+        this.eachWidth = 128; // Width of each frame in pixels
+        this.eachHeight = 128; // Height of each frame in pixels
         this.numFrames = 4; // Total number of frames in the spritesheet
     }
 
     draw(ctx) {
-        // Calculate the x and y coordinates of the current frame in the spritesheet
-        const sx = this.currentFrame * this.frameWidth;
+        const sx = this.currentFrame * this.eachWidth;
         const sy = 0;
 
-        // Draw the current frame of the projectile on the canvas
-        ctx.drawImage(this.spriteSheet, sx, sy, this.frameWidth, this.frameHeight, this.pos[0], this.pos[1], this.frameWidth, this.frameHeight);
-
-        // Increment the current frame index
+        if (this.vel[1] < 0){
+            ctx.drawImage(this.userProjectileSheet, sx, sy, this.eachWidth, this.eachHeight, this.pos[0], this.pos[1], 60, 60);    
+        } else {
+            
+            ctx.drawImage(this.enemyProjectileSheet, sx, sy, this.eachWidth, this.eachHeight, this.pos[0], this.pos[1], 60, 60);
+        }
         this.currentFrame = (this.currentFrame + 1) % this.numFrames;
     }
 }
